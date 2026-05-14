f11f8e8add6daa17d6a6f0a445c15331977f32bd
GitHub Action <action@github.com>
2026-05-14 10:11:38 +0000

    Template cleanup

--- src/main/kotlin/com/github/focacciasyin/opencodeconnector/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/focacciasyin/opencodeconnector/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.focacciasyin.opencodeconnector.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
